9354453a1448ae130bc5397cbc4e103751edc0d4
TechMaster2099 <127751338+TechMaster2099@users.noreply.github.com>
2025-09-10 10:55:42 +0300

    modified  modified:   generate_password.php

--- generate_password.php
+++ generate_password.php
@@ -1,4 +1,8 @@
 <?php
+  $password="Reagan23;;";
+  $hash=password_hash($password,PASSWORD_DEFAULT);
+  echo "Password:". $password."<br>";
+  echo "Hashed Password: <br><textarea rows='2' cols='80'>$hash</textarea>";
 
 
 ?>
