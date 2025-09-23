c214504f2814feabeea7f85ae733f2408f50d72a
TechMaster2099 <127751338+TechMaster2099@users.noreply.github.com>
2025-09-23 12:14:24 +0300

    modification

---
+++ staff_dashboard.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Staff dashboard</title>
+</head>
+<body>
+    <h2>staff dashboard</h2>
+<div class="staff_links">
+    <a href="create_loans.php">Create loan</a>
+</div>    
+    <br><br>
+<div class="staff_links">    
+    <a href="record_return.php">Record return</a>
+</div>   
+    <br><br>
+<div class="staff_links">
+    <a href="view_assets.php">View assets</a>
+</div>   
+</body>
+</html>
